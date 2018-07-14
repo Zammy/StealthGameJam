d578e2c58ed6f3f7addfce316ab7e0057099b663
kpacuaeon <kpacuaeon@gmail.com>
2018-07-14 19:05:16 +0300

    submitted Materials/Hat.mat and CameraChaser

---
+++ StealthGame/Assets/Scripts/CameraChaser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChaser : MonoBehaviour {
+
+    public Transform Target;
+    public float speed = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+        GameObject.FindWithTag("")
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = Vector3.Lerp(this.transform.position, Target.position, speed);	
+	}
+}
