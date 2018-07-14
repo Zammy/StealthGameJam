9a4cbb59386ad0200e7d5810bc1158801e982d96
kpacuaeon <kpacuaeon@gmail.com>
2018-07-15 00:25:11 +0300

    refactored Player

--- StealthGame/Assets/Scripts/AI/Player.cs
+++ StealthGame/Assets/Scripts/AI/Player.cs
@@ -13,27 +13,44 @@ public class Player : MonoBehaviour
 
     [Header("Settings")]
     public float EyesightFOV = 90;
-    public float EyesightDistance = 25f;
+    public float EyesightDistance = 25;
     public float StealthSpeed = 1;
     public float RunSpeed = 2.5f;
     public float clickTime = .25f;
 
 
     [Header("References")]
+    public Collider BodyCollider;
     public Transform Head;
+    public GameObject LookAhead;
+    public GameObject LookatTarget;
 
     private Vector3 LastPos;
     private float Speed;
+    private float lastClickTime = 0;
+    private int ClickCounter=0;
 
+    
+    
+    public bool LookatMode = false;
+    
     void Start()
     {
         var entityContainer = GetComponent<IEntityContainer>();
         entityContainer.AddEntity(new NavMeshAgentEntity(GetComponent<NavMeshAgent>()));
         entityContainer.AddEntity(new PhysicalEntity(transform));
         entityContainer.AddEntity(new EyesightEntity(EyesightFOV, EyesightDistance, Head));
+        entityContainer.AddEntity(new SpottableEntity(BodyCollider));
+
 
         ServiceLocator.Instance.GetService<IVisibilitySystem>().AddPlayer(entityContainer);
-   }
+
+        LookatTarget = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        LookatTarget.name = "LookatPos";
+        LookatTarget.GetComponent<SphereCollider>().enabled = false;
+        LookatTarget.transform.localScale = new Vector3(0.3f,0.3f,0.3f);
+        LookatTarget.transform.position = LookAhead.transform.position;
+    }
 
     Coroutine _goto;
 
@@ -41,19 +58,22 @@ public class Player : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(1))
         {
-            if (StealthMode)
-            {
-                GetComponent<NavMeshAgent>().speed = RunSpeed;
-                GetComponent<Animator>().SetBool("Stealth", false);
+           
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo, float.MaxValue, LayerMask.GetMask("Ground")))
+            {   
+                LookatTarget.transform.position = new Vector3(hitInfo.point.x, Head.transform.position.y, hitInfo.point.z);
             }
-            else
-            {
-                GetComponent<NavMeshAgent>().speed = StealthSpeed;
-                GetComponent<Animator>().SetBool("Stealth", true);
-            }
-                
+        }
 
-            StealthMode = !StealthMode;
+        if (LookatMode)
+        {
+            Head.transform.LookAt(LookatTarget.transform.position);
+        }
+        else
+        {   
+            
         }
 
         Speed = Vector3.Distance(LastPos, this.transform.position) / Time.deltaTime;
@@ -72,9 +92,25 @@ public class Player : MonoBehaviour
         NoiseLevel = 100 * (StealthMode ? 0.3f : 1) * (Moving ? 1 : 0);
 
 
-
         if (Input.GetMouseButtonDown(0))
         {
+            if (Time.time - lastClickTime < clickTime && ClickCounter == 1)
+            {
+                ClickCounter = 2;
+            }
+            else
+            {
+                ClickCounter = 1;
+            }
+
+            lastClickTime = Time.time;
+        }
+
+        if (ClickCounter == 2)
+        {
+            GetComponent<NavMeshAgent>().speed = RunSpeed;
+            GetComponent<Animator>().SetBool("Stealth", false);
+
             if (_goto != null)
             {
                 StopCoroutine(_goto);
@@ -88,7 +124,33 @@ public class Player : MonoBehaviour
                 var destination = hitInfo.point;
                 _goto = StartCoroutine(Move.Do(GetComponent<IEntityContainer>(), destination));
             }
+
+            Debug.Log("Double click.");
+            ClickCounter = 0;
         }
+
+        if (ClickCounter == 1 && Time.time - lastClickTime > clickTime)
+        {
+            GetComponent<NavMeshAgent>().speed = StealthSpeed;
+            GetComponent<Animator>().SetBool("Stealth", true);
+
+            if (_goto != null)
+            {
+                StopCoroutine(_goto);
+                _goto = null;
+            }
+
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo, float.MaxValue, LayerMask.GetMask("Ground")))
+            {
+                var destination = hitInfo.point;
+                _goto = StartCoroutine(Move.Do(GetComponent<IEntityContainer>(), destination));
+            }
+            Debug.Log("Single Click.");
+            ClickCounter = 0;
+        }
+        
 
         LastPos = this.transform.position;
     }
