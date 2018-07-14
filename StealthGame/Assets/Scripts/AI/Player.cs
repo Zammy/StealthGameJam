5fb995c97b05af169f4111fce732b447d3bbf688
kpacuaeon <kpacuaeon@gmail.com>
2018-07-14 17:52:03 +0300

    added Player

---
+++ StealthGame/Assets/Scripts/AI/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player: MonoBehaviour
+{
+    void Start()
+    {
+        var entityContainer = GetComponent<IEntityContainer>();
+        entityContainer.AddEntity(new NavMeshAgentEntity(GetComponent<NavMeshAgent>()));
+        entityContainer.AddEntity(new PhysicalEntity(transform));
+    }
+
+    Coroutine _goto;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (_goto != null)
+            {
+                StopCoroutine(_goto);
+                _goto = null;
+            }
+
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo, float.MaxValue, LayerMask.GetMask("Ground")))
+            {
+                var destination = hitInfo.point;
+                Debug.LogFormat("destination {0}", destination);
+                StartCoroutine(Test(destination));
+            }
+        }
+    }
+
+    IEnumerator Test(Vector3 destination)
+    {
+        _goto = StartCoroutine(Move.Do(GetComponent<IEntityContainer>(), destination));
+        yield return _goto;
+        Debug.Log("Reached");
+    }
+}
